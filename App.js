d515d8121cee8f2665a1b2147d176d2ef2389213
Android Studio <devandroid@ort>
2025-05-12 10:10:18 -0300

    Creo que vf

--- App.js
+++ App.js
@@ -1,40 +1,55 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Image, ImageBackground, Pressable, TouchableOpacity, StatusBar, Alert } from "react-native";
+import React, { useState, useRef} from "react";
+import { StyleSheet, Text, View, TextInput, Image, ImageBackground, Pressable, TouchableOpacity, StatusBar, Alert, PanResponder, Animated } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useFonts } from 'expo-font';
 
 export default function App() {
   const [mensaje, setMensaje] = useState("");
+  const [presionado, setPresionado] = useState(false)
 
-  const contactar = () => {
-    Alert.alert("Mensaje enviado", mensaje);
+  const cambiarColor = () => {
+    setPresionado(!presionado); // Alternar el valor de presionado
   };
-
-  const verPerfil = () => {
-    Alert.alert("Perfil", "Nombre: Juan Pérez\nTítulo: Desarrollador Frontend");
+  const contactar = () => {
+    if(mensaje==""){
+      Alert.alert("Mensaje enviado", "No se recibió ningún mensaje");
+    }else {Alert.alert("Mensaje enviado", mensaje);}
   };
 
    const [fuentes] = useFonts({
-    'Poppins': require("./assets/fonts/Poppins-Black.ttf")
+    "Poppins": require("./assets/fonts/Poppins-Black.ttf"),
+    "PoppinsMedium": require("./assets/fonts/Poppins-Medium.ttf")
   });
+  const pan = useRef(new Animated.ValueXY()).current;
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event(
+        [null, { dx: pan.x, dy: pan.y }],
+        { useNativeDriver: false } 
+      ),
+      onPanResponderRelease: () => {
+        Animated.spring(pan, {
+          toValue: { x: 0, y: 0 },
+          useNativeDriver: false, 
+        }).start();
+      },
+    })
+  ).current;
+  
   return (
     <ImageBackground source={require("./assets/background.jpg")} style={styles.imagenFondo}>
       <StatusBar barStyle="light-content"/>
       <SafeAreaView style={styles.safeContainer}>
       <View style={styles.ViewGeneral}>
-        <View style={styles.container}>
+        <Animated.View style={[styles.container, {transform: [{translateX: pan.x}, {translateY: pan.y}] }]} {...panResponder.panHandlers} >
           <Image source={require("./assets/aaa.png")} style={styles.profileImage} />
-          <Text style={styles.nombre}>Uriel Jolodovsky</Text>
-          <Text style={styles.titulo}>Desarrollador FullStack</Text>
-          <TextInput style={styles.input} placeholder="Escribí tu mensaje..." value={mensaje} onChangeText={setMensaje}/>
-          <TouchableOpacity style={styles.boton} onPress={contactar}>
-            <Text style={styles.textoBoton}>Contactar</Text>
-          </TouchableOpacity>
-
-          <Pressable style={({ pressed }) => [styles.verPerfil, { backgroundColor: pressed ? "#cde" : "#eef" }]} onPress={verPerfil}>
-            <Text style={styles.textoVerPerfil}>Ver Perfil</Text>
-          </Pressable>
-        </View>
+          <Text style={styles.nombre}>Ian Berman</Text>
+          <Text style={styles.titulo}>Sales Manager</Text>
+          <TextInput style={styles.input} placeholder="Ingresá tu mensaje :)" value={mensaje} onChangeText={setMensaje}/>
+          <TouchableOpacity style={styles.boton} onPress={contactar}><Text style={styles.textoBoton}>Contactar</Text></TouchableOpacity>
+          <Pressable onPress={cambiarColor}><Text style={[styles.textoVerPerfil, presionado && styles.textoPresionado]}>Ver Perfil</Text></Pressable>
+      </Animated.View>
       </View>
       </SafeAreaView>
     </ImageBackground>
@@ -44,7 +59,6 @@ export default function App() {
 const styles = StyleSheet.create({
   imagenFondo: {
     flex: 1,
-    resizeMode: "cover"
   },
   safeContainer: {
     flex: 1
@@ -74,25 +88,28 @@ const styles = StyleSheet.create({
     color: "black"
   },
   titulo: {
-    fontSize: 15,
+    fontFamily: "PoppinsMedium",
+    fontSize: 13,
     color: "black",
     marginBottom: 30
   },
   input: {
+    fontFamily: "PoppinsMedium",
     width:"80%",
     borderRadius: 10,
     backgroundColor: "#fff",
     marginBottom: 15
   },
   boton: {
-    backgroundColor: "lightblue",
+    backgroundColor: "#004987",
     padding: 10,
     borderRadius: 8,
     marginBottom: 10
   },
   textoBoton: {
+    fontFamily: "PoppinsMedium",
     fontSize:15,
-    color: "black",
+    color: "white", 
     fontWeight: "bold"
   },
   verPerfil: {
@@ -100,7 +117,15 @@ const styles = StyleSheet.create({
     borderRadius: 8
   },
   textoVerPerfil: {
+    borderRadius: 5,
+    backgroundColor: "black",
+    fontFamily: "PoppinsMedium",
     fontWeight: "bold",
-    color: "black"
+    fontSize: 18,
+    color: "white", 
+  },
+  textoPresionado: {
+    color: "white",
+    backgroundColor: "grey"
   }
 });
